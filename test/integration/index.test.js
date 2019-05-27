58037ee00487a395d087dd04ede5cf7c672da408
valecelerier <valecelerier@hotmail.com>
2019-05-27 18:01:10 -0300

    Test#3 Probar que se elimina una peli

--- test/integration/index.test.js
+++ test/integration/index.test.js
@@ -19,3 +19,41 @@ test('Se debería iniciar la aplicación sin películas', async () => {
 
     expect(movies.length).toBe(0)
 });
+
+test('Se deberia poder eliminar una pelicula de la bd', async () => { 
+    //Se declara la pelicula
+    const movie = {
+        title: 'Back to the Future',
+        description: 'Marty McFly, a 17-year-old high school student, is accidentally sent thirty years into the past in a time-traveling DeLorean invented by his close friend, the maverick scientist Doc Brown.',
+        year: 1985,
+        runtime: 116,
+        country: 'United States',
+        language: 'English',
+        genres: ['Adventure', 'Comedy', 'Science Fiction'],
+        directors: ['Robert Zemeckis'],
+        writers: ['Robert Zemeckis', 'Bob Gale']
+        };
+
+    //Se llama a la API
+    await fetch(`${baseURL}/movies`, {
+        method: 'POST', 
+        body: JSON.stringify(movie),
+        headers:{
+            'Content-Type': 'application/json'
+        }});
+
+
+
+    await fetch(`${baseURL}/movies/1`, {
+        method: 'DELETE', 
+        headers:{
+            'Content-Type': 'application/json'
+        }});    
+
+
+    const allMovies = await fetch(`${baseURL}/movies`)
+        .then(result => result.json());
+
+    
+    expect(allMovies.length).toBe(0);
+});
